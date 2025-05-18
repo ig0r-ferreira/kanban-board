dc371fbc4586cc1d660405c63e8fa42c44116e5d
Igor Ferreira <iferreira.n01@gmail.com>
2025-05-18 16:35:47 -0300

    set API rate limit to 60 req/min and disable in tests

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->api(prepend: [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            'throttle:api',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
