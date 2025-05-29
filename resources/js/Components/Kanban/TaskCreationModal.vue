4e117ec1d8bd31c3d740b55527653eb4a1ac6dea
Igor Ferreira <iferreira.n01@gmail.com>
2025-05-28 22:58:59 -0300

    refactor to use the modal that comes with Laravel Breeze

--- resources/js/Components/Kanban/TaskCreationModal.vue
+++ resources/js/Components/Kanban/TaskCreationModal.vue
@@ -1,88 +1,84 @@
 <template>
-  <div
-    v-if="show"
-    class="fixed inset-0 z-50 flex items-center justify-center bg-black/50 overflow-y-auto"
-  >
-    <div class="bg-white rounded-lg p-6 w-full max-w-lg shadow-lg">
-      <Vueform endpoint="api/tasks" method="post" @response="handleResponse">
-        <StaticElement name="create_title" content="Create task" tag="h3" />
-        <StaticElement name="divider" tag="hr" />
-        <TextElement
-          name="title"
-          label="Title"
-          placeholder="What is it about?"
-          :floating="false"
-          rules="required|string|max:255"
-        />
-        <TextareaElement
-          name="description"
-          label="Description"
-          :floating="false"
-          placeholder="Add details, context or steps to reproduce..."
-          rules="required"
-          :rows="10"
-        />
-        <SelectElement
-          name="priority"
-          placeholder="Priority"
-          default="Medium"
-          rules="required"
-          :native="false"
-          :items="['Highest', 'High', 'Medium', 'Low', 'Lowest']"
-          :columns="6"
-        />
-        <DateElement
-          name="due_date"
-          floating="Due date"
-          placeholder="Select a due date..."
-          rules="required|date|after_or_equal:today"
-          :min="new Date().toJSON().slice(0, 10)"
-          :columns="6"
-        />
-        <SelectElement
-          name="reporter_id"
-          placeholder="Reporter"
-          rules="required"
-          label-prop="name"
-          value-prop="id"
-          :default="defaultReporter"
-          :native="false"
-          :items="users"
-          :search="true"
-        />
-        <SelectElement
-          name="assignee_id"
-          placeholder="Assignee"
-          rules="required"
-          label-prop="name"
-          value-prop="id"
-          :native="false"
-          :items="users"
-          :search="true"
-        />
-        <StaticElement name="divider" tag="hr" />
-        <ButtonElement
-          secondary
-          name="cancel"
-          add-class="mt-2"
-          :columns="6"
-          full
-          @click="$emit('close')"
-        >
-          Cancel
-        </ButtonElement>
-        <ButtonElement name="save" add-class="mt-2" :columns="6" full submits>
-          Save
-        </ButtonElement>
-      </Vueform>
-    </div>
-  </div>
+  <Modal :show="show" max-width="lg">
+    <Vueform endpoint="api/tasks" method="post" @response="handleResponse">
+      <StaticElement name="create_title" content="Create task" tag="h3" />
+      <StaticElement name="divider" tag="hr" />
+      <TextElement
+        name="title"
+        label="Title"
+        placeholder="What is it about?"
+        :floating="false"
+        rules="required|string|max:255"
+      />
+      <TextareaElement
+        name="description"
+        label="Description"
+        :floating="false"
+        placeholder="Add details, context or steps to reproduce..."
+        rules="required"
+        :rows="10"
+      />
+      <SelectElement
+        name="priority"
+        placeholder="Priority"
+        default="Medium"
+        rules="required"
+        :native="false"
+        :items="['Highest', 'High', 'Medium', 'Low', 'Lowest']"
+        :columns="6"
+      />
+      <DateElement
+        name="due_date"
+        floating="Due date"
+        placeholder="Select a due date..."
+        rules="required|date|after_or_equal:today"
+        :min="new Date().toJSON().slice(0, 10)"
+        :columns="6"
+      />
+      <SelectElement
+        name="reporter_id"
+        placeholder="Reporter"
+        rules="required"
+        label-prop="name"
+        value-prop="id"
+        :default="defaultReporter"
+        :native="false"
+        :items="users"
+        :search="true"
+      />
+      <SelectElement
+        name="assignee_id"
+        placeholder="Assignee"
+        rules="required"
+        label-prop="name"
+        value-prop="id"
+        :native="false"
+        :items="users"
+        :search="true"
+      />
+      <StaticElement name="divider" tag="hr" />
+      <ButtonElement
+        secondary
+        name="cancel"
+        add-class="mt-2"
+        :columns="6"
+        full
+        @click="closeModal"
+      >
+        Cancel
+      </ButtonElement>
+      <ButtonElement name="save" add-class="mt-2" :columns="6" full submits>
+        Save
+      </ButtonElement>
+    </Vueform>
+  </Modal>
 </template>
 
 <script setup>
 import { ref, watch, computed } from "vue";
 import axios from "axios";
 import { usePage } from "@inertiajs/vue3";
+import Modal from "@/Components/Modal.vue";
 
 const { auth } = usePage().props;
 const props = defineProps({
@@ -112,12 +108,17 @@ const handleResponse = (response, form$) => {
   emit("created");
 };
 
+const closeModal = () => {
+  emit("close");
+};
+
 watch(
   () => props.show,
   (newVal) => {
     if (newVal) {
       fetchUsers();
     }
-  }
+  },
+  { immediate: true }
 );
 </script>
--- resources/js/Components/Kanban/TaskDetailsModal.vue
+++ resources/js/Components/Kanban/TaskDetailsModal.vue
@@ -1,175 +1,163 @@
 <template>
-  <div
-    v-if="show"
-    class="fixed inset-0 z-50 flex items-center justify-center bg-black/50 overflow-y-auto"
-  >
-    <div
-      class="bg-white rounded-lg p-3 w-full max-w-lg shadow-lg flex flex-col"
-    >
-      <div class="flex justify-end">
-        <button
-          v-if="!isEditing"
-          class="py-2 px-4 rounded-md bg-sky-800 text-white text-sm transition hover:scale-110"
-          @click="isEditing = true"
-        >
-          Editar
-        </button>
-      </div>
-      <div class="p-3">
-        <Vueform v-if="!isEditing">
-          <StaticElement name="title" :content="task.title" tag="h3" />
-          <StaticElement
-            name="key"
-            :content="`#${task.key}`"
-            tag="p"
-            add-class="font-bold"
-          />
-          <StaticElement name="divider" tag="hr" />
-          <StaticElement
-            name="description"
-            label="Description"
-            :content="task.description"
-            tag="p"
-          />
-          <StaticElement
-            name="status"
-            label="Status"
-            :content="task.status.name"
-          />
-          <StaticElement
-            name="priority"
-            label="Priority"
-            :content="task.priority"
-            :columns="6"
-          />
-          <StaticElement
-            name="due_date"
-            label="Due date"
-            :content="`${isPastDate(task.due_date) ? '🚨' : ''} ${task.due_date}`"
-            :columns="6"
-            :add-class="{
-              content: { 'text-red-600': isPastDate(task.due_date) },
-            }"
-          />
-          <StaticElement
-            name="reporter"
-            label="Reporter"
-            :content="task.reporter.name"
-            :columns="6"
-          />
-          <StaticElement
-            name="assignee"
-            label="Assignee"
-            :content="task.assignee.name"
-            :columns="6"
-          />
-          <ButtonElement
-            secondary
-            name="close"
-            add-class="mt-8 mx-auto w-full sm:w-1/2"
-            full
-            @click="closeModal"
-          >
-            Close
-          </ButtonElement>
-        </Vueform>
-        <Vueform v-else ref="form$" :endpoint="false" @submit="onSubmit">
-          <StaticElement name="edit_title" content="Edit task" tag="h3" />
-          <StaticElement name="divider" tag="hr" />
-          <TextElement
-            name="title"
-            label="Title"
-            placeholder="What is it about?"
-            :floating="false"
-            rules="required|string|max:255"
-          />
-          <TextareaElement
-            name="description"
-            label="Description"
-            :floating="false"
-            placeholder="Add details, context or steps to reproduce..."
-            rules="required"
-            :rows="10"
-          />
-          <SelectElement
-            name="status_id"
-            placeholder="Status"
-            rules="required"
-            label-prop="name"
-            value-prop="id"
-            :native="false"
-            :items="
-              statuses.map((status) => ({
-                ...status,
-                disabled: status.name === task.status.name,
-              }))
-            "
-            :disabled="
-              statuses &&
-              statuses.length > 0 &&
-              statuses[statuses.length - 1].name === task.status.name
-            "
-          />
-          <SelectElement
-            name="priority"
-            placeholder="Priority"
-            default="Medium"
-            rules="required"
-            :native="false"
-            :items="['Highest', 'High', 'Medium', 'Low', 'Lowest']"
-            :columns="6"
-          />
-          <DateElement
-            name="due_date"
-            floating="Due date"
-            placeholder="Select a due date..."
-            rules="required|date|after_or_equal:today"
-            :min="new Date().toJSON().slice(0, 10)"
-            :columns="6"
-          />
-          <SelectElement
-            name="reporter_id"
-            placeholder="Reporter"
-            rules="required"
-            label-prop="name"
-            value-prop="id"
-            :native="false"
-            :items="users"
-            :search="true"
-          />
-          <SelectElement
-            name="assignee_id"
-            placeholder="Assignee"
-            rules="required"
-            label-prop="name"
-            value-prop="id"
-            :native="false"
-            :items="users"
-            :search="true"
-          />
-          <StaticElement name="divider" tag="hr" />
-          <ButtonElement
-            secondary
-            name="cancel"
-            add-class="mt-2"
-            :columns="6"
-            full
-            @click="closeModal()"
-          >
-            Cancel
-          </ButtonElement>
-          <ButtonElement name="save" add-class="mt-2" :columns="6" full submits>
-            Save
-          </ButtonElement>
-        </Vueform>
-      </div>
+  <Modal :show="show" max-width="lg">
+    <div class="flex justify-end">
+      <button
+        v-if="!isEditing"
+        class="py-2 px-4 rounded-md bg-sky-800 text-white text-sm transition hover:scale-110"
+        @click="isEditing = true"
+      >
+        Editar
+      </button>
     </div>
-  </div>
+    <Vueform v-if="!isEditing">
+      <StaticElement name="title" :content="task.title" tag="h3" />
+      <StaticElement
+        name="key"
+        :content="`#${task.key}`"
+        tag="p"
+        add-class="font-bold"
+      />
+      <StaticElement name="divider" tag="hr" />
+      <StaticElement
+        name="description"
+        label="Description"
+        :content="task.description"
+        tag="p"
+      />
+      <StaticElement name="status" label="Status" :content="task.status.name" />
+      <StaticElement
+        name="priority"
+        label="Priority"
+        :content="task.priority"
+        :columns="6"
+      />
+      <StaticElement
+        name="due_date"
+        label="Due date"
+        :content="`${isPastDate(task.due_date) ? '🚨' : ''} ${task.due_date}`"
+        :columns="6"
+        :add-class="{
+          content: { 'text-red-600': isPastDate(task.due_date) },
+        }"
+      />
+      <StaticElement
+        name="reporter"
+        label="Reporter"
+        :content="task.reporter.name"
+        :columns="6"
+      />
+      <StaticElement
+        name="assignee"
+        label="Assignee"
+        :content="task.assignee.name"
+        :columns="6"
+      />
+      <ButtonElement
+        secondary
+        name="close"
+        add-class="mt-8 mx-auto w-full sm:w-1/2"
+        full
+        @click="closeModal"
+      >
+        Close
+      </ButtonElement>
+    </Vueform>
+    <Vueform v-else ref="form$" :endpoint="false" @submit="onSubmit">
+      <StaticElement name="edit_title" content="Edit task" tag="h3" />
+      <StaticElement name="divider" tag="hr" />
+      <TextElement
+        name="title"
+        label="Title"
+        placeholder="What is it about?"
+        :floating="false"
+        rules="required|string|max:255"
+      />
+      <TextareaElement
+        name="description"
+        label="Description"
+        :floating="false"
+        placeholder="Add details, context or steps to reproduce..."
+        rules="required"
+        :rows="10"
+      />
+      <SelectElement
+        name="status_id"
+        placeholder="Status"
+        rules="required"
+        label-prop="name"
+        value-prop="id"
+        :native="false"
+        :items="
+          statuses.map((status) => ({
+            ...status,
+            disabled: status.name === task.status.name,
+          }))
+        "
+        :disabled="
+          statuses &&
+          statuses.length > 0 &&
+          statuses[statuses.length - 1].name === task.status.name
+        "
+      />
+      <SelectElement
+        name="priority"
+        placeholder="Priority"
+        default="Medium"
+        rules="required"
+        :native="false"
+        :items="['Highest', 'High', 'Medium', 'Low', 'Lowest']"
+        :columns="6"
+      />
+      <DateElement
+        name="due_date"
+        floating="Due date"
+        placeholder="Select a due date..."
+        rules="required|date|after_or_equal:today"
+        :min="new Date().toJSON().slice(0, 10)"
+        :columns="6"
+      />
+      <SelectElement
+        name="reporter_id"
+        placeholder="Reporter"
+        rules="required"
+        label-prop="name"
+        value-prop="id"
+        :native="false"
+        :items="users"
+        :search="true"
+      />
+      <SelectElement
+        name="assignee_id"
+        placeholder="Assignee"
+        rules="required"
+        label-prop="name"
+        value-prop="id"
+        :native="false"
+        :items="users"
+        :search="true"
+      />
+      <StaticElement name="divider" tag="hr" />
+      <ButtonElement
+        secondary
+        name="cancel"
+        add-class="mt-2"
+        :columns="6"
+        full
+        @click="closeEditing"
+      >
+        Cancel
+      </ButtonElement>
+      <ButtonElement name="save" add-class="mt-2" :columns="6" full submits>
+        Save
+      </ButtonElement>
+    </Vueform>
+  </Modal>
 </template>
 
 
 <script setup>
-import { ref, watch, onMounted } from "vue";
+import { ref, watch } from "vue";
+import Modal from "@/Components/Modal.vue";
 import axios from "axios";
 import { isPastDate } from "@/Utils/date";
 
@@ -178,8 +166,9 @@ const props = defineProps({
   task: Object,
 });
 const emit = defineEmits(["close", "updated"]);
-const isEditing = ref(false);
 const form$ = ref(null);
+const isEditing = ref(false);
+const isSubmitted = ref(false);
 const statuses = ref([]);
 const users = ref([]);
 
@@ -201,16 +190,23 @@ async function fetchStatuses(query, input) {
   }
 }
 
-const closeModal = () => {
+const closeEditing = () => {
   isEditing.value = false;
+};
+
+const closeModal = () => {
   emit("close");
+  if (isSubmitted.value) {
+    emit("updated");
+    isSubmitted.value = false;
+  }
 };
 
 const handleResponse = (response, form$) => {
-  emit("updated", response.data);
   form$.messageBag.clear();
   form$.reset();
-  closeModal();
+  closeEditing();
+  isSubmitted.value = true;
 };
 
 const onSubmit = async (form$) => {
